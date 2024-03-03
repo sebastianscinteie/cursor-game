5e21148e6a66138f2b60ad0b068b49bf8d056651
sebastianscinteie <sebastian.scinteie@gmail.com>
2024-03-03 16:17:23 +0200

    Add whisper cus whi not?

--- server.ts
+++ server.ts
@@ -1,55 +1,69 @@
-import { type UUID, randomUUID } from "crypto";
-import { WebSocketServer } from "ws";
+import { type RawData, type WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 9000 });
 interface Users {
-  [index: UUID]: {
-    username?: string;
+  [username: string]: {
     online: boolean;
     state?: {
       X: number;
       Y: number;
     };
+    webSocket: WebSocket | null;
   };
 }
 const users: Users = {};
 
-function broadcast(wss: WebSocketServer): void {
+function broadcast(wss: WebSocketServer, message: string): void {
   wss.clients.forEach((client) => {
-    client.send(JSON.stringify(users));
+    client.send(message);
   });
 }
 
 wss.on("connection", (ws, req) => {
   ws.on("error", console.error);
 
-  const uuid = randomUUID();
-  console.log(uuid);
-  const un = new URL(
+  const username = new URL(
     req.url as string,
     `http://${req.headers.host}`,
-  ).searchParams.get("username");
+  ).searchParams.get("username") as string;
 
-  users[uuid] = {
-    username: un as string,
-    online: true,
-    state: {
-      X: 0,
-      Y: 0,
-    },
-  };
-  const user = users[uuid];
+  const user = users[username];
+  if (user !== undefined) {
+    if (user?.webSocket !== null) {
+      user.webSocket = ws;
+    } else {
+      console.error("User %s already connected.", username);
+    }
+  } else {
+    users[username] = {
+      online: true,
+      state: {
+        X: 0,
+        Y: 0,
+      },
+      webSocket: ws,
+    };
+  }
 
-  ws.on("message", (data) => {
+  ws.on("message", (data: RawData) => {
     console.log("received: %s", data);
+    const textMessage = data.toLocaleString();
+    if (textMessage[0] === "@") {
+      const userToWhisper = textMessage
+        .split(" ", 1)[0]
+        ?.substring(1) as string;
+      users[userToWhisper]?.webSocket?.send(textMessage);
+    } else {
+      broadcast(wss, textMessage);
+    }
   });
 
   ws.on("close", () => {
     if (user !== undefined) {
       user.online = false;
     }
-    broadcast(wss);
+    broadcast(wss, JSON.stringify(users));
   });
 
-  broadcast(wss);
+  broadcast(wss, JSON.stringify(users));
 });
